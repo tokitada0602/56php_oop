6c8cae71117f866202d406ea8cc8982432a2f85f
西河時忠 <tolitada2406@gmail.com>
2019-10-29 09:50:14 +0800

    index.phpにヘッダー、検索ツール追加。create.php追加

--- index.php
+++ index.php
@@ -11,6 +11,26 @@
 
 </head>
 <body>
-  
+<header class="px-5 bg-primary">
+        <nav class="navbar navbar-dark">
+            <a href="index.php" class="navbar-brand">TODO APP</a>
+            <div class="justify-content-end">
+                <span class="text-light">
+                    SeedKun
+                </span>
+            </div>
+        </nav>
+    </header>
+    <main>
+    <main class="container py-5">
+        <section>
+            <form class="form-row justify-content-center" action="create.php" method="POST">
+                <div class="col-10 col-md-6 py-2">
+                    <input type="text" class="form-control" placeholder="ADD TODO" name="task">
+                </div>
+                <div class="py-2 col-md-3 col-10">
+                    <button type="submit" class="col-12 btn btn-primary">ADD</button>
+                </div>
+            </form>
 </body>
 </html>
