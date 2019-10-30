1c750da7f3546042cb9a4ae0e11b07c907a92c5c
西河時忠 <tolitada2406@gmail.com>
2019-10-30 10:14:57 +0800

    index.php "edit"にlink(edit.php)を作成

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<!DOCTYPE html>
+<?php
+    require_once('function.php');
+    require_once('Models/Todo.php');
+    // todo クラスのインスタンス化
+    $todo = new Todo();
+// DBからデータを全件取得
+    $tasks = $todo->all()
+    // echo <'pre'>;
+    // var_dump($tasks);
+?>
+  <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
@@ -12,25 +22,60 @@
 </head>
 <body>
 <header class="px-5 bg-primary">
-        <nav class="navbar navbar-dark">
-            <a href="index.php" class="navbar-brand">TODO APP</a>
-            <div class="justify-content-end">
-                <span class="text-light">
-                    SeedKun
-                </span>
+    <nav class="navbar navbar-dark">
+        <a href="index.php" class="navbar-brand">TODO APP</a>
+        <div class="justify-content-end">
+            <span class="text-light">
+                SeedKun
+            </span>
+        </div>
+    </nav>
+</header>
+
+<main class="container py-5">
+    <section>
+        <form class="form-row justify-content-center" action="create.php" method="POST">
+            <div class="col-10 col-md-6 py-2">
+                <input type="text" class="form-control" placeholder="ADD TODO" name="task">
             </div>
-        </nav>
-    </header>
-    <main>
-    <main class="container py-5">
-        <section>
-            <form class="form-row justify-content-center" action="create.php" method="POST">
-                <div class="col-10 col-md-6 py-2">
-                    <input type="text" class="form-control" placeholder="ADD TODO" name="task">
-                </div>
-                <div class="py-2 col-md-3 col-10">
-                    <button type="submit" class="col-12 btn btn-primary">ADD</button>
-                </div>
-            </form>
+            <div class="py-2 col-md-3 col-10">
+                <button type="submit" class="col-12 btn btn-primary">ADD</button>
+            </div>
+        </form>
+    </section>
+    <section class="mt-5">
+        <table class="table table-hover">
+            <thead>
+            <tr class="bg-primary text-light">
+                <th class=>TODO</th>
+                <th>DUE DATE</th>
+                <th>STATUS</th>
+                <th></th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($tasks as $task):?>
+                    <tr>
+                    <td>
+                    <?php echo $task['name']; ?>
+                    </td>
+                    <td>
+                    <?php echo $task['due_date']; ?>
+                    </td>
+                    <td>NOT YET</td>
+                    <td>
+                    <a class="text-success" href="edit.php?id=<?php echo h($task['id']); ?>">EDIT</a>
+                </td>
+                <?php
+    var_dump($task['id']);
+?>
+                <td>
+                    <a class="text-danger" href="">DELETE</a>
+                </td>
+                    <?php endforeach; ?>
+            </tbody>
+        </table>
+    </section>
 </body>
 </html>
